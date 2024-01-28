9a4e11c7a050008985509cf483befb92d69d2422
Charanjit Singh Bhalla <bhallacharanjit@gmail.com>
2024-01-28 15:51:08 +0530

    Added additional routes

--- Controllers/movies.js
+++ Controllers/movies.js
@@ -23,4 +23,52 @@ const getSingleMovie = async(req, res) => {
     }
 };
 
-module.exports = { getAllMovies, getSingleMovie };
+const createMovie = async(req, res) => {
+    try {
+        const { id, title, year, rating } = req.body;
+        const newMovie = { id, title, year, rating };
+        allMovies.push(newMovie);
+        res.status(201).send(newMovie);
+    } catch (error) {
+        res.status(404).send(error.message);
+    }
+}
+
+
+const updateMovie = async(req, res) => {
+    try {
+        const { id } = req.params;
+        const { title, year, rating } = req.body;
+        const movie = allMovies.find((movie) => String(movie.id) === String(id));
+        if (movie) {
+            if (title) movie.title = title;
+            if (year) movie.year = year;
+            if (rating) movie.rating = rating;
+            res.status(200).send(movie);
+        } else {
+            res.status(404).send("Movie not found");
+        }
+    } catch (error) {
+        res.status(404).send(error.message);
+    }   
+}
+
+const deleteMovie = async(req, res) => {
+    try {
+        const { id } = req.params;
+        const movie = allMovies.find((movie) => String(movie.id) === String(id));
+        if (movie) {
+            const index = allMovies.indexOf(movie);
+            allMovies.splice(index, 1);
+            res.status(200).send(movie);
+        } else {
+            res.status(404).send("Movie not found");
+        }
+    } catch (error) {
+        res.status(404).send(error.message);
+    }
+}
+    
+
+
+module.exports = { getAllMovies, getSingleMovie, createMovie, updateMovie, deleteMovie};
--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -6,7 +6,9 @@ app.get("/movies", getAllMovies);
 
 //Get single movie
 app.get("/movies/:id", getSingleMovie)
-
+app.post("/movies", createMovie);
+app.put("/movies/:id", updateMovie);
+app.delete("/movies/:id", deleteMovie);
 
 
 module.exports = app;
